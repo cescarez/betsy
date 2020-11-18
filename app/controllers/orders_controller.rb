3e028a448f3e9e95d3c5d372f399f786ce90645f
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-17 19:11:48 -0800

    Added order params and created outline for each action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,32 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+  end
+  def new
+    @order = Order.new
+  end
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      render file: "#{Rails.root}/public/not_found.html", status: :not_found
+    end
+
+  end
+  def show
+
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def destroy
+
+  end
+  private
+  def order_params
+    return require(:order).permit(:user_id, :order_item_id, :shipping_info_id, :billing_info_id, :completed)
+  end
 end
