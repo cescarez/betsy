17529508f5b04bc1582b0ae175d52ec97b7b93f0
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-17 21:29:59 -0800

    Added checkout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  get "/orders/:id/checkout", to: "orders#checkout", as: "checkout_order"
   post "/orders/:id/complete", to: "orders#complete", as: "complete_order"
   resources :orders
 end
