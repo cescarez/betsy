0576027e9ce5a32a068901058cacc22f819c86b1
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-17 20:00:28 -0800

    Finished basic CRUD mehtods and strong params.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,22 +14,67 @@ class ProductsController < ApplicationController
       return
     end
   end
+
   def new
     @product = Product.new
   end
 
   def create
-    @work = Work.new(work_params)
-    puts @work.inspect
-    puts @work.valid?
-    if @work.save
-      redirect_to works_path
-      flash[:success] = "#{@work.title} was successfully added!"
+    @product = Product.new(product_params)
+    puts @product.inspect
+    puts @product.valid?
+    if @product.save
+      redirect_to products_path
+      flash[:success] = "#{@product.title} was successfully added!"
       return
     else
-      flash.now[:error] = "Something happened. Media not added."
+      flash.now[:error] = "Something happened. Product not added."
       render :new, status: :bad_request
       return
     end
   end
+
+  def edit
+    @product = Work.find_by(id: params[:id])
+    if @product.nil?
+      head :not_found
+      flash[:error] = "Cannot find this product."
+      return
+    end
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    if @product.nil?
+      head :not_found
+      flash.now[:error] = "Something happened. Media not updated."
+      return
+    elsif @product.update(product_params)
+      flash[:success] = "#{@product.title} was successfully updated!"
+      redirect_to products_path
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+
+    if @product.nil?
+      head :not_found
+      return
+    else
+      @product.destroy
+      flash[:success] = "#{@product.title} was deleted"
+      redirect_to products_path
+      return
+    end
+  end
+
+  private
+  def product_params
+    return params.require(:product).permit(:category, :name, :price, :description, :inventory, :user_id)
+  end
+end
 end
