6254855d7575538bc79b8d632cb7f6a79e3d27ea
codeandcodemore <alicedilanchian@gmail.com>
2020-11-18 11:52:07 -0500

    routes for users updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,11 @@ Rails.application.routes.draw do
   # Omniauth callback route
   get "/auth/:provider/callback", to: "users#create", as:"omniauth_callback"
 
-  get 'users/create'
-  get 'users/index'
-  get 'users/show'
-  get 'users/update'
-  get 'users/destroy'
-  get 'users/edit'
-  get 'users/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 
+  delete "/logout", to: "users#destroy", as: "logout"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :products
 
 end
