ae9eea4ea497477f82690b0e1489c6fe274f643d
marks214 <aimee.oz.x@gmail.com>
2020-11-18 11:15:01 -0800

    added validations and build_from_github method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_many :products # add scope so just logged in user has products
+  validates :username, uniqueness: true, presence: true
+  validates :uid, uniqueness: {scope: :provider}
+
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash["uid"]
+    user.provider = 'github'
+    user.username = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
+    user.avatar = auth_hash["info"]["image"]
+    return user
+  end
 end
