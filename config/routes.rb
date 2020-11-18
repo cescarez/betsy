5a8939a83ea16f0be2552254397946b7ca2ad9c5
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-18 06:38:23 -0800

    included both restful routes for Products and Orders, along with custom routes for Orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   resources :products
 
+  get "orders/:id/checkout", to: "orders#checkout", as: "checkout_order"
+  post "/orders/:id/complete", to: "orders#complete", as: "complete_order"
+  resources :orders
 end
