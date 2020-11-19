cf6c2c73465cf8c1b8e3b07f304e83079d360b7e
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-19 12:43:36 -0800

    renamed cvv attribute to card_cvv

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_155103) do
+ActiveRecord::Schema.define(version: 2020_11_19_203922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_155103) do
     t.string "card_brand"
     t.date "card_expiration"
     t.string "card_number"
-    t.string "cvv"
+    t.string "card_cvv"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "order_id"
