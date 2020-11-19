708cfd1b664d1eea9cf25cbc1d1cddd406a7a691
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-19 07:50:53 -0800

    changed has_one relations to has_many for shipping and billing_info

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,8 @@ VALID_STATUSES = ["pending", "paid", "complete", "cancelled"]
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
-  has_one :shipping_info
-  has_one :billing_info
+  has_many :shipping_info
+  has_many :billing_info
 
   validates_date :submit_date, on_or_before: lambda { Date.current }
   validates_date :complete_date, on_or_before: lambda { Date.current }
