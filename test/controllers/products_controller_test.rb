04363912bf351a283919254c218113aa3ffe0a6c
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-19 09:16:47 -0800

    added a destroy test that isn't passing

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -5,8 +5,15 @@ describe ProductsController do
     it "creates a new product" do
       @user = users(:user_1)
 
-      expect {@product = Product.new(category: "planet", name: "Saturn", price: 24.95, description: "It's got rings!", inventory: 5, user_id: @user.id)}.must_change 'Product.count', 1
+      expect {@product = Product.new(category: "planet", name: "Saturn", price: 24.95, description: "It's got rings!", inventory: 5, user_id: @user.id)}.must_change(Product, :count).by(+1)
 
+    end
+  end
+  describe "destroy products" do
+    it "removes a destroyed product from the database" do
+      @product = products(:product_2)
+
+      expect { @product.destroy }.to change(Product, :count).by(-1)
     end
   end
 end
