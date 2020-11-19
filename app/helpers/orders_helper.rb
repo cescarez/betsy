e05e602af23c613e518d955a03eff58eab9d3d4a
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-18 19:24:03 -0800

    Added table in show view for purchaser info

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,7 +1,11 @@
 module OrdersHelper
   def hide_card(card_num)
-    hidden_num =  "*" * card_num.length
-    hidden_num[-4..-1] = card_num[-4..-1]
+    if card_num.length > 4
+      hidden_num =  "*" * card_num.length
+      hidden_num[-4..-1] = card_num[-4..-1]
+    else
+      hidden_num = card_num
+    end
     return hidden_num
   end
 end
