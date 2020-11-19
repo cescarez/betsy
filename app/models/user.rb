bd3bd2e5b7184a23b8418b68faf20ff45a77c850
marks214 <aimee.oz.x@gmail.com>
2020-11-19 12:57:08 -0800

    total earnings method in user model, drafted user index and show views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   has_many :products # add scope so just logged in user has products
+  has_many :order_items, through: :products
   validates :username, uniqueness: true, presence: true
   validates :uid, uniqueness: {scope: :provider}
+  validates :email, :provider, presence: :true
 
   def self.build_from_github(auth_hash)
     user = User.new
@@ -12,4 +14,22 @@ class User < ApplicationRecord
     user.avatar = auth_hash["info"]["image"]
     return user
   end
+
+  def total_earnings
+    status = %w[cancelled pending]
+    gross_earnings = 0
+    sold_products = self.order_items
+    if sold_products.length > 0
+      sold_products.each do |sold_product|
+        unless sold_product.status.include?(status)
+          gross_earnings += sold_product.quantity * sold_product.price
+        end
+      end
+      return gross_earnings
+    else
+      return gross_earnings
+    end
+  end
+
+
 end
