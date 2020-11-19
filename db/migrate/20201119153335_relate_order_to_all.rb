9096c56acfc4361b0dcb0509e47659e41db66580
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-19 07:44:31 -0800

    removed shipping_info and billing_info to order. unknown error

--- db/migrate/20201119153335_relate_order_to_all.rb
+++ db/migrate/20201119153335_relate_order_to_all.rb
@@ -2,11 +2,7 @@ class RelateOrderToAll < ActiveRecord::Migration[6.0]
   def change
     add_reference :orders, :user, index: true
     add_reference :orders, :order_items, index: true
-    add_reference :orders, :shipping_info, index: true
-    add_reference :orders, :billing_info, index: true
     add_reference :users, :order, index: true
     add_reference :order_items, :order, index: true
-    add_reference :shipping_info, :order, index: true
-    add_reference :billing_info, :order, index: true
   end
 end
