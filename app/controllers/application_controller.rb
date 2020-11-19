c8122e1cb0c526fdce4c783cdc984525a5910873
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-18 20:53:58 -0800

    Reverted commenting out and restored global "require_login"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
-  # before_action :require_login
+  before_action :require_login
 
   def render_404
     return render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
