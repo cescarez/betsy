b54a6b6bf6fe861f0eeffb48a4a0714c83331c8d
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-19 07:48:43 -0800

    related order to users and order_items (pending relations to shipping_info and billing_info)

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_060719) do
+ActiveRecord::Schema.define(version: 2020_11_19_153335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "billing_infos", force: :cascade do |t|
+    t.string "card_brand"
+    t.date "card_expiration"
+    t.string "card_number"
+    t.string "cvv"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "product_id"
     t.integer "quantity"
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "status"
+    t.datetime "submit_date"
+    t.datetime "complete_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.bigint "order_items_id"
+    t.index ["order_items_id"], name: "index_orders_on_order_items_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "shipping_infos", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,6 +68,8 @@ ActiveRecord::Schema.define(version: 2020_11_19_060719) do
     t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_users_on_order_id"
   end
 
 end
