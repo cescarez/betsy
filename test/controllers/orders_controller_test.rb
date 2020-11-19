087a89e37f3036b622b4e4dd4ad41613f4def1d5
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-18 20:17:19 -0800

    wrote index test, but need to change routes file before proceeding

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,12 +1,27 @@
 require "test_helper"
 
 describe OrdersController do
+  let (:order_hash) do
+    {
+      order: {
+        user: users(:user_1),
+        order_item_id: 1, #change to OrderItems
+        shipping_info_id: 1, #change to ShippingInfo
+        billing_info_id: 1, #change to BillingInfo
+        status: "pending",
+        submit_date: Time.now - 3.days,
+        complete_date: Time.now,
+      }
+    }
+  end
+
   describe "index" do
-
+    it "responds with a success code" do
+      get orders_path
+      must_respond_with :success
+    end
   end
-  describe "new" do
 
-  end
   describe "create" do
 
   end
