bf626d95031e55c7b355a300d967cc3610db78ff
marks214 <aimee.oz.x@gmail.com>
2020-11-19 16:02:20 -0800

    changed presence to false for uid requirement - so seed data would work :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :products # add scope so just logged in user has products
   has_many :order_items, through: :products
   validates :username, uniqueness: true, presence: true
-  validates :uid, uniqueness: {scope: :provider}
+  validates :uid, uniqueness: {scope: :provider}, presence: false
   validates :email, :provider, presence: :true
 
   def self.build_from_github(auth_hash)
