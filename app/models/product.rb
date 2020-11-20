55938720c379fd9e39e94bd5b24ee56b2f4dc8bd
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-19 16:32:32 -0800

    validation models for product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   validates :category, presence: true
   validates :name, presence: true
   validates :price, presence: true
-  validates :inventory, presence: true
+  validates :inventory, presence:true, numericality: { only_integer:true, greater_than: 0 }
   validates :user_id, presence: true
   validates :name, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 2020_11_19_060719) do
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "category"
+    t.string "name"
+    t.decimal "price"
+    t.text "description"
+    t.integer "inventory"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "username"
