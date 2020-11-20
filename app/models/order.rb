3ecc2cc95c1875c6a05c60aded3b9d5a61536c1a
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-19 16:41:24 -0800

    Changed billing_info structure and changed fixtures so has_many attributes are plural

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,8 @@ VALID_STATUSES = ["pending", "paid", "complete", "cancelled"]
 class Order < ApplicationRecord
   belongs_to :user, optional: true
   has_many :order_items
-  has_many :shipping_info, optional: true
-  has_many :billing_info, optional: true
+  has_many :shipping_info
+  has_many :billing_info
 
   validates :status, presence: true
   validates_date :submit_date, on_or_before: :today, allow_nil: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_001610) do
+ActiveRecord::Schema.define(version: 2020_11_20_003848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,13 +23,6 @@ ActiveRecord::Schema.define(version: 2020_11_20_001610) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "order_id"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "street"
-    t.string "city"
-    t.string "state"
-    t.string "zipcode"
-    t.string "country"
     t.index ["order_id"], name: "index_billing_infos_on_order_id"
   end
 
