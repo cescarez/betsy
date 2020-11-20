fea76c4c6615d00343bc1842d56a398542146ce8
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-19 17:53:19 -0800

    returning correct value in validate_quantity

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,10 +4,10 @@ class OrderItem < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
   def validate_quantity
-    if self.quantity >= self.product.inventory
+    if self.quantity > self.product.inventory
       self.errors.add(:quantity, "Number of #{self.product.name} added to card exceeds seller's in-stock inventory(#{self.product.inventory}). Please enter a smaller quantity.")
     else
-      return self.card_brand
+      return self.quantity
     end
   end
 
