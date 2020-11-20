8f93adc7269809111cabfee03bee7b43da3395c4
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-19 16:59:25 -0800

    related user to products with a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_004314) do
+ActiveRecord::Schema.define(version: 2020_11_20_005733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,9 +65,10 @@ ActiveRecord::Schema.define(version: 2020_11_20_004314) do
     t.decimal "price"
     t.text "description"
     t.integer "inventory"
-    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
   end
 
   create_table "shipping_infos", force: :cascade do |t|
