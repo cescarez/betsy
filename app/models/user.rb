6d7ab46a749eab3b35a8d14fbdd7e833b28eb8d5
codeandcodemore <alicedilanchian@gmail.com>
2020-11-21 10:19:45 -0500

    texts fixing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :products # add scope so just logged in user has products
-  has_many :order_items, through: :products #should be through :orders instead of :products?
+  has_many :order_items, through: :orders #should be through :orders instead of :products? both are currently giving errors
   validates :username, uniqueness: true, presence: true
   validates :uid, uniqueness: {scope: :provider}, presence: false
   validates :email, :provider, presence: :true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,48 +23,50 @@ describe User do
   end
 
   describe "validations" do
+    before do
+      @user = User.new(uid: 12345, username: "Username", provider: "github", email: "email@address.com")
+    end
 
     it "must have a username" do
-      user = User.new
-      expect(user.valid?).must_equal false
-      expect(user.errors.messages).must_include :username
+      @user.username = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :username
     end
     it "must have a uid" do
-      user = User.new
-      expect(user.valid?).must_equal false
-      expect(user.errors.messages).must_include :uid
+      @user.uid = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :uid
     end
     it "must have an email" do
-      user = User.new
-      expect(user.valid?).must_equal false
-      expect(user.errors.messages).must_include :email
+      @user.email = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :email
       end
     it "must have a provider" do
-      user = User.new
-      expect(user.valid?).must_equal false
-      expect(user.errors.messages).must_include :provider
+      @user.provider = nil
+      expect(@user.valid?).must_equal false
+      expect(@user.errors.messages).must_include :provider
     end
     it "must have a unique username" do
-      username = "Username"
-      user = User.new(username: username)
-      user.save!
+      @user.save!
+      username = @user.username
       user_copy = User.new(username: username)
       result = user_copy.save
       expect(result).must_equal false
       expect(user_copy.errors.messages).must_include :username
     end
     it "must have a unique uid through provider" do
-      uid = "12345"
-      user = User.new(uid: uid, provider: "github")
-      user.save!
-      user_copy = User.new(uid: uid, provider: "github")
+      @user.save!
+      uid = @user.uid
+      provider = @user.provider
+      user_copy = User.new(uid: uid, provider: provider)
       result = user_copy.save
       expect(result).must_equal false
       expect(user_copy.errors.messages).must_include :uid
     end
     it "must be valid when created with all the required fields" do
-      user = User.new(username: "Username", uid: "12345", provider: "github", email: "email@address.com")
-      expect(user.valid?).must_equal true
+      #user = User.new(username: "Username", uid: "12345", provider: "github", email: "email@address.com")
+      expect(@user.valid?).must_equal true
     end
 
   end
@@ -80,7 +82,6 @@ describe User do
               #avatar:
           }
       }
-
       user = User.build_from_github(auth_hash)
       user.save!
       expect(User.count).must_equal 1
@@ -89,21 +90,19 @@ describe User do
       expect(user.username).must_equal auth_hash[:info][:name]
       expect(user.email).must_equal auth_hash[:info][:email]
     end
-
-
-    end
-
-  describe "total_probable_earnings" do
-
-
-
   end
 
-  describe "total_actual_earnings" do
-
-
-
-  end
+  # describe "total_probable_earnings" do
+  #
+  #
+  #
+  # end
+  #
+  # describe "total_actual_earnings" do
+  #
+  #
+  #
+  # end
 
 
 end
