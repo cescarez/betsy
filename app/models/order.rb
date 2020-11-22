ebf2b4a81320f50c958d92681d8c9c861a45b268
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-22 13:18:04 -0800

    change shipping and billing infos from has_many to has_one relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,8 @@ VALID_STATUSES = ["pending", "paid", "complete", "cancelled"]
 class Order < ApplicationRecord
   # belongs_to :user, optional: true
   has_many :order_items, dependent: :destroy
-  has_many :shipping_infos
-  has_many :billing_infos
+  has_one :shipping_info, dependent: delete
+  has_one :billing_info, dependent: delete
 
   validates_date :submit_date, on_or_before: :today, allow_nil: true
   validates_date :complete_date, on_or_before: :today, on_or_after: :submit_date, allow_nil: true
