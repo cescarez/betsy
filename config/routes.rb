22e1227e4bb965de08c09fb5ae845bfe95ce066d
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-22 17:55:18 -0800

    Fixed order item call, added cancelled button, fixed calls to access purchases information

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ Rails.application.routes.draw do
 
   post "/orders/status", to: "orders#status_filter", as: "order_status_filter"
 
-  resources :shipping_infos, except: [:index]
-  resources :billing_infos, except: [:index]
+  resources :shipping_infos, only: [:new, :edit, :show]
+  resources :billing_infos, only: [:new, :edit, :show]
 
   get "/orders/:id/checkout", to: "orders#checkout", as: "checkout_order"
   post "/orders/:id/checkout", to: "orders#submit"
