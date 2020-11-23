1917174f87d98d45ed0e738d2b0b18dee42d34ed
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-22 18:25:15 -0800

    assigning user.name as the same as user.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     user = User.new
     user.uid = auth_hash["uid"]
     user.provider = 'github'
+    user.name = auth_hash["info"]["name"]
     user.username = auth_hash["info"]["name"]
     user.email = auth_hash["info"]["email"]
     user.avatar = auth_hash["info"]["image"]
