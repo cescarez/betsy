ac5f709c2a6133cddc12144f4feacee40a4e8f81
codeandcodemore <alicedilanchian@gmail.com>
2020-11-24 00:15:04 -0500

    routes dried up

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,21 @@
 Rails.application.routes.draw do
 
   root to: "users#index"
-
   get "/users/current", to: "users#current", as: "current_user"
-
-  # Omniauth login route
   get "/auth/github", as: "github_login"
-
-  # Omniauth callback route
   get "/auth/:provider/callback", to: "users#create", as: "omniauth_callback"
-
-  resources :users
-
   delete "/logout", to: "users#destroy", as: "logout"
+  resources :users, only: [:index, :show]
 
-  resources :products do
+  resources :products, except: [:destroy] do
     resources :reviews, only: [:create, :new]
   end
-
   patch "products/:id/add_to_cart", to: 'products#add_to_cart', as: 'add_to_cart'
   patch "products/:id/retire", to: 'products#set_retire', as: 'retire'
+
   resources :categories, except: [:edit, :update, :destroy]
 
   post "/orders/status", to: "orders#status_filter", as: "order_status_filter"
-
-  resources :shipping_infos, except: [:index]
-  resources :billing_infos, except: [:index]
-
-
   patch "/orders", to: 'orders#create', as: 'create_cart'
   get "/orders/:id/checkout", to: "orders#checkout", as: "checkout_order"
   get "/orders/:id/summary", to: "orders#summary", as: "order_summary"
@@ -36,5 +24,7 @@ Rails.application.routes.draw do
   post "/orders/:id/cancel", to: "orders#cancel", as: "cancel_order"
   resources :orders, except: [:new, :edit, :destroy]
 
+  resources :shipping_infos, except: [:index]
+  resources :billing_infos, except: [:index]
 
 end
