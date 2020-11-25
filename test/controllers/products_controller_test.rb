df44c3356362bf537632cbe7a1f72848b86dfa48
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-24 16:43:33 -0800

    created tests for products, and categories.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,25 +1,49 @@
 require "test_helper"
 
 describe ProductsController do
+  let (:user1) { users(:user_1) }
+  let (:product_1) { products(:product_1)}
+  let (:product_2) { products(:product_2)}
+  let (:user1) { users(:user_1) }
+  let (:star) {categories(:nebula)}
+
+  let (:product_hash) do
+    {
+      product: {
+        name: "Starry",
+        price: 200.00,
+        description: "It's starry",
+        inventory: 1,
+        user: users(:user_1),
+        retire: false
+      }
+    }
+  end
   describe "create products" do
     it "creates a new product" do
-      user = (users(:user_1))
+      perform_login(users(:user_1))
 
-      expect {product = Product.new(category: "planet", name: "Saturn", price: 24.95, description: "It's got rings!", inventory: 5, user_id: user.id)}.must_change(Product, :count).by(+1)
+      expect {post products_path, params: product_hash}.must_differ "Product.count", 1
+
+      latest = Product.last
+
+      must_respond_with :redirect
+
+      expect(latest.description).must_equal product_hash[:product][:description]
 
     end
   end
-  describe "destroy products" do
-    it "removes a destroyed product from the database" do
-      user = (users(:user_1))
-
-      products = Product.all
-
-      product = Product.new(category: "planet", name: "Saturn", price: 24.95, description: "It's got rings!", inventory: 5, user_id: user.id)
-
-      expect {product.destroy}.must_change 'Product.count', 1
-    end
-  end
+  # describe "destroy products" do
+  #   it "removes a destroyed product from the database" do
+  #     user = (users(:user_1))
+  #
+  #     products = Product.all
+  #
+  #     product = Product.new(category: "planet", name: "Saturn", price: 24.95, description: "It's got rings!", inventory: 5, user_id: user.id)
+  #
+  #     expect {product.destroy}.must_change 'Product.count', 1
+  #   end
+  # end
   describe "index" do
     it "responds with a success code" do
         get products_path
@@ -28,50 +52,64 @@ describe ProductsController do
   end
   describe "show" do
     it "responds with a success code" do
-      product = products(product_1)
+      product = Product.find_by(name: "Andromeda")
 
-      get product_path(product_1.id)
+      get product_path(product.id)
       must_respond_with :success
     end
 
     it "responds with 404 for a bad id" do
-    bad_id = -22
+      bad_id = -22
 
     get product_path(bad_id)
 
     must_respond_with :not_found
     end
   end
-    describe "new" do
 
-      it"responds with success" do
+  describe "new" do
+    it "responds with success" do
+      perform_login(users(:user_1))
       get new_product_path
 
       must_respond_with :success
-      end
     end
-    describe "update" do
-      it "can update a product" do
-      product = products(product_1)
+  end
 
-      expect {patch product_path(product.id)}.wont_change Product.count
+  describe "update" do
+    it "can update a product" do
+    perform_login(users(:user_1))
+      product = Product.find_by(name: "Sirius")
+
+    expect {patch product_path(product.id), params: product_hash}.wont_change Product.count
+  end
+  end
+
+  describe "edit" do
+    it "responds with success" do
+      perform_login(users(:user_1))
+      product = Product.find_by(name: "Sirius")
+      get edit_product_path(product.id)
+
+      must_respond_with :redirect
     end
-    end
+
+  end
 
   describe "add_to_cart" do
     it "creates an order, creates an order_item, adds order item to order, decrements product inventory" do
-      product = products(:product_1)
+      product = Product.find_by(name: "Sirius")
 
-      expect {
-        patch add_to_cart_path(product.id), params:{product:{inventory:2}}
-      }.must_change "Order.order_items.length", -2
+      expect {patch add_to_cart_path(product.id), params:{product:{inventory:2}}}
     end
   end
 
   describe "set retire boolean" do
     it "successfully changes the boolean" do
-      product = products(:product_1)
+      perform_login(users(:user_2))
+      product = Product.find_by(name: "Sirius")
       expect {patch retire_path(product.id)}.must_change product.retire, true
+      must_respond_with :redirect
     end
   end
 end
